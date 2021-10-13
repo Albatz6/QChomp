bf793ca16e573f8d1e1b3b042070df5d8291b4be
Konstantin Luzgin <Konstantin.Luzgin59@gmail.com>
2021-10-13 20:06:45 +0500

    Created basic page layout, grid component via table and AJAX-post for making a move

--- WebQChomp/Pages/Index.cshtml.cs
+++ WebQChomp/Pages/Index.cshtml.cs
@@ -1,25 +1,104 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Antiforgery;
+using QChompLibrary;
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WebQChomp.Pages
 {
+    public class Input
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _appEnvironment;
+        private Field _gameField = new Field(6, 9, (0, 0));
+        private AI _easyModel = Train(500, 6, 9);
+        private AI _mediumModel = Train(1000, 6, 9);
+        private AI _hardModel = Train(2500, 6, 9);
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IWebHostEnvironment appEnvironment)
         {
-            _logger = logger;
+            _appEnvironment = appEnvironment;
+            //_model = AI.LoadModel(Path.Combine(_appEnvironment.WebRootPath, "6_9_1713_modelv1.dat"));
         }
 
-        public void OnGet()
+        public JsonResult OnPostAction([FromBody]Input json)
         {
+            _gameField.MakeMove((json.X, json.Y));
+            (int Height, int Width) action = _easyModel.ChooseAction(_gameField.Grid, true);
+            return new JsonResult(new { Height = action.Height, Width = action.Width });
+        }
 
+        static AI Train(int iterations, int h, int w)
+        {
+            int delta = 0;
+            AI player = new AI();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                // Initialize new game
+                Field game = new Field(h, w, (0, 0));
+
+                // Keeping track of last move made by both players
+                Dictionary<int, (int[,] State, (int Height, int Width) Action)> last;
+                last = new Dictionary<int, (int[,] State, (int Height, int Width) Action)>();
+
+                last[(int)Field.Players.Player1] = (default, default);
+                last[(int)Field.Players.Player2] = (default, default);
+
+                // Game loop
+                while (true)
+                {
+                    // Keep current state and action
+                    int[,] state = new int[game.GridHeight, game.GridWidth];
+                    Array.Copy(game.Grid, 0, state, 0, game.Grid.Length);
+                    (int Height, int Width) action = player.ChooseAction(game.Grid, true);
+
+                    // Keep last state and action
+                    last[game.Player] = (state, action);
+
+                    // Make move
+                    game.MakeMove(action);
+                    int[,] newState = game.Grid;
+
+                    // Update q-values with rewards and break after the game is over, otherwise proceed with no reward
+                    if (game.Winner != (int)Field.Players.Blank)
+                    {
+                        player.UpdateModel(state, action, newState, -1);
+                        if (last[game.Player].State != null)
+                            player.UpdateModel(last[game.Player].State, last[game.Player].Action, newState, 1);
+
+                        break;
+                    }
+                    else if (last[game.Player].State != null) // Default state value is null
+                    {
+                        player.UpdateModel(last[game.Player].State, last[game.Player].Action, newState, 0);
+                    }
+                }
+
+                // Training stats output
+                if ((i + 1) % 1000 == 0)
+                {
+                    int transitions = player.Transitions;
+
+                    Console.WriteLine($"Training game {((i + 1) / 1000)}k... ({transitions} transitions, {transitions - delta} delta)");
+                    delta = player.Transitions;
+                }
+            }
+
+            Console.WriteLine("Done training\n");
+            return player;
         }
     }
 }
--- WebQChomp/wwwroot/js/site.js
+++ WebQChomp/wwwroot/js/site.js
@@ -2,3 +2,91 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Represents game difficulty
+// 0 - easy, 1 - medium, 2 - hard
+var difficulty = 0;
+var userMove = true;
+
+
+// Handle difficulty radio button group switching
+var selector = document.getElementById('difficulty-selection');
+selector.addEventListener('click', ({ target }) => {
+    if (target.getAttribute('name') === 'diff-radio') {
+        difficulty = parseInt(target.value);
+        console.log("Difficulty:", difficulty);
+
+        // TODO: reset game board and switch model
+    }
+});
+
+// Create grid and handle it
+var lastClicked;
+var grid = clickableGrid(6, 9, function (el, row, col) {
+    console.log("You clicked on element:", el);
+    console.log("You clicked on row:", row);
+    console.log("You clicked on col:", col);
+
+    el.className = 'clicked';
+    if (lastClicked) lastClicked.className = '';
+    lastClicked = el;
+
+    if (userMove) {
+        userMove = false;
+        var move = makeMove(row, col);
+    }
+});
+
+document.body.appendChild(grid);
+
+// Grid forming function
+function clickableGrid(rows, cols, callback) {
+    var grid = document.createElement('table');
+    grid.className = 'grid';
+
+    for (var r = 0; r < rows; ++r) {
+        var tr = grid.appendChild(document.createElement('tr'));
+        tr.style.height = '35px';
+
+        for (var c = 0; c < cols; ++c) {
+            var cell = tr.appendChild(document.createElement('td'));
+            cell.id = `cell-${r * cols + c}`;
+            cell.style.width = '35px';
+
+            if (r === 0 && c === 0) cell.bgColor = 'red';
+
+            cell.addEventListener('click', (function (el, r, c) {
+                return function () {
+                    callback(el, r, c);
+                }
+            })(cell, r, c), false);
+        }
+    }
+
+    return grid;
+}
+
+// AJAX request that sends the user move and returns AI's
+function makeMove(row, col) {
+    var move = []
+    var token = document.getElementById('RequestVerificationToken').value;
+
+    fetch('/?handler=Action', {
+        method: 'POST',
+        headers: {
+            "RequestVerificationToken": token,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            x: row,
+            y: col
+        })
+    })
+        .then(response => response.json)
+        .then(result => {
+            console.log(result);
+            // TODO: retrieve AI's move coords from response
+        }) 
+
+    return move;
+}
